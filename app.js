9daf721ef585d555d2e6999f4c2467e4505e036f
Maddy246 <29030663+Maddy246@users.noreply.github.com>
2021-08-25 12:37:18 +0530

    html updaed

---
+++ app.js
@@ -0,0 +1,8 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+function clickHandler(){
+    console.log("Clicked!")
+}
+
+btnTranslate.addEventListener("click", clickHandler);
